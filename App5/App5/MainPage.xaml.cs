6278dbe115befb0ecbb53d04d0f0079afdba40c4
yas15 <31019957+yas15@users.noreply.github.com>
2017-10-22 22:10:48 +0100

    Add project files.

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel vm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            vm = new MainPageViewModel();
+
+            // set the BindingContext Property of the view MainPage to a new instance of the MainPage ViewModel.
+            // i.e. set the default datasource for all the objects in the MainPage View to the objects in the MainPageViewModel.
+            BindingContext = vm;
+
+            // Xamarin.Forms.MessagingCenter allows a developer to send and process events easily in code.
+            // MessagingCenter methods include: -
+            // Send: Send will raise an event. It publishes a message on which a listener can act.
+            // Subscribe: Subscribe will listen for messages that have a certain signature. 
+            // Multiple listeners can subscribe to the same message signature.
+            // Unsubscribe: Unsubscribe will stop a listener from responding to messages with the defined signature.
+            MessagingCenter.Subscribe<MainPageViewModel>(vm, "ButtonClicked", (sender) =>
+            {
+                DisplayAlert("Message", "Button Clicked!", "OK");
+            });
+
+        }
+    }
+}
---
+++ App5/App5/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+
+namespace App5
+{
+    class MainPageViewModel
+    {
+        public ICommand ButtonClickedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            ButtonClickedCommand = new Command(HandleButtonClick);
+        }
+
+        private void HandleButtonClick()
+        {
+            MessagingCenter.Send<MainPageViewModel>(this, "ButtonClicked");
+
+        }
+    }
+}
